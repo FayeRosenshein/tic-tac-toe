02b1295ad0af98001e807912de2bc92567ede08f
Faye Rosenshein <rosefaye.a@gmail.com>
2022-11-08 19:39:57 -0700

    feat: Continue to write in game.js file

--- src/game.js
+++ src/game.js
@@ -3,10 +3,37 @@ class Game {
         this.playerOne = gameInfo.playerOne
         this.playerTwo = gameInfo.playerTwo
         this.gameBoard = []
-        for(var i = 0; i < 9; i++) {
+        for (var i = 0; i < 9; i++) {
             this.gameBoard.push('')
         }
         this.currentPlayer = this.playerOne
+        this.hasWon = false
+        this.draw = false
+    }
+    checkEndCondition() {
+        var winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        for (var i = 0; i < winConditions.length; i++) {
+            var triplet = winConditions[i];
+           if (this.gameBoard[triplet[0]] === this.gameBoard[triplet[1]] &&
+            this.gameBoard[triplet[0]] === this.gameBoard[triplet[2]]) {
+                this.hasWon = true
+            }
+        }
+        var hasEmpty = this.gameBoard.includes('')
+        if (!hasEmpty) {
+            this.draw = true
+        }
+    }
+    resetGameBoard() {
+        this.gameBoard = []
+        for (var i = 0; i < 9; i++) {
+            this.gameBoard.push('')
+        }
+        this.hasWon = false
+        this.draw = false
+    }
+    playTurn() {
+        this.checkEndCondition()
     }
 }
 module.exports = Game
--- test/game-test.js
+++ test/game-test.js
@@ -1,21 +1,23 @@
-// A Game should include:
-// Two Player instances x
-// A way to keep track of the data for the game board x
-// A way to keep track of which player’s turn it currently is x
-// A way to check the Game’s board data for win conditions 
-// A way to detect when a game is a draw (no one has won)
-// A way to reset the Game’s board to begin a new game
+// player should be able to play a turn
+// player1 can win, draw, lose
+//player2 can win, draw, lose
+//player can only pick elements 0-8
+//player can't pick an element that has already been picked
+//player can't pick an element once game is over
+//
 
-var assert = require("chai").assert;
-var Game = require("../src/game");
-var Player = require("../src/player");
+
+
+var assert = require("chai").assert
+var Game = require("../src/game")
+var Player = require("../src/player")
 
 describe("Game", function () {
     it('Should have 2 players', function () {
         var newPlayer1 = new Player({ id: 'one', token: '1' });
         var newPlayer2 = new Player({ id: 'two', token: '2' });
 
-        var newGame = new Game({playerOne: newPlayer1, playerTwo: newPlayer2})
+        var newGame = new Game({ playerOne: newPlayer1, playerTwo: newPlayer2 })
 
         assert.equal(newGame.playerOne, newPlayer1)
         assert.equal(newGame.playerTwo, newPlayer2)
@@ -24,7 +26,7 @@ describe("Game", function () {
         var newPlayer1 = new Player({ id: 'one', token: '1' });
         var newPlayer2 = new Player({ id: 'two', token: '2' });
 
-        var newGame = new Game({playerOne: newPlayer1, playerTwo: newPlayer2})
+        var newGame = new Game({ playerOne: newPlayer1, playerTwo: newPlayer2 })
 
         assert.equal(newGame.gameBoard.length, 9)
     })
@@ -32,16 +34,71 @@ describe("Game", function () {
         var newPlayer1 = new Player({ id: 'one', token: '1' });
         var newPlayer2 = new Player({ id: 'two', token: '2' });
 
-        var newGame = new Game({playerOne: newPlayer1, playerTwo: newPlayer2})
+        var newGame = new Game({ playerOne: newPlayer1, playerTwo: newPlayer2 })
 
         assert.equal(newGame.currentPlayer, newGame.playerOne)
     })
     it('Should be able to tell win conditions', function () {
         var newPlayer1 = new Player({ id: 'one', token: '1' });
         var newPlayer2 = new Player({ id: 'two', token: '2' });
+        var winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        var newGame = new Game({ playerOne: newPlayer1, playerTwo: newPlayer2 })
 
-        var newGame = new Game({playerOne: newPlayer1, playerTwo: newPlayer2})
+        for (var i = 0; i < winConditions.length; i++) {
+            var triplet = winConditions[i]
+            for (var j = 0; j < triplet.length; j++) {
+                newGame.gameBoard[triplet[j]] = newPlayer1.id
+            }
+            newGame.checkEndCondition()
+            assert.equal(newGame.hasWon, true)
+        }
+    })
+    it('Should know when the game is a draw', function () {
+        var newPlayer1 = new Player({ id: 'one', token: '1' });
+        var newPlayer2 = new Player({ id: 'two', token: '2' });
+        var newGame = new Game({ playerOne: newPlayer1, playerTwo: newPlayer2 })
 
+        for (var i = 0; i < newGame.gameBoard.length; i++) {
+            newGame.gameBoard[i] = i
+        }
+
+        newGame.checkEndCondition()
+
+        assert.equal(newGame.hasWon, false)
+        assert.equal(newGame.draw, true)
+    })
+    it('Should reset the game\'s board', function () {
+        var newPlayer1 = new Player({ id: 'one', token: '1' });
+        var newPlayer2 = new Player({ id: 'two', token: '2' });
+        var newGame = new Game({ playerOne: newPlayer1, playerTwo: newPlayer2 })
         
+        for (var i = 0; i < newGame.gameBoard.length; i++) {
+            newGame.gameBoard[i] = i
+        }
+        
+        newGame.checkEndCondition()
+        newGame.resetGameBoard()
+
+        assert.equal(newGame.gameBoard.length, 9)
+        for (var i = 0; i < 9; i++) {
+            assert.equal(newGame.gameBoard[i], '')
+        }
+        assert.equal(newGame.hasWon, false)
+        assert.equal(newGame.draw, false)
+
+        newGame.gameBoard[0] = newPlayer1.id
+        newGame.gameBoard[1] = newPlayer1.id
+        newGame.gameBoard[2] = newPlayer1.id
+        
+        newGame.checkEndCondition()
+        assert.equal(newGame.hasWon, true)
+        newGame.resetGameBoard()
+
+        assert.equal(newGame.gameBoard.length, 9)
+        for (var i = 0; i < 9; i++) {
+            assert.equal(newGame.gameBoard[i], '')
+        }
+        assert.equal(newGame.hasWon, false)
+        assert.equal(newGame.draw, false)
     })
 })
